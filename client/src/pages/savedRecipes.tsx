aa3b71d58241786f2698f43d48831733c874adac
payton <payton@paytons-Air.lan>
2025-03-06 17:44:05 -0700

    checking to commit

--- client/src/pages/savedRecipes.tsx
+++ client/src/pages/savedRecipes.tsx
@@ -43,6 +43,7 @@ const SavedRecipe = () => {
     const User = auth.getProfile();
     if (User.id !== null && User.id !== undefined) {
       savedRecipeAPI.deleteRecipe(id, recipe, User.id);
+      window.location.reload();
     }
   }
   const GenerateSavedRecipes = () => {
